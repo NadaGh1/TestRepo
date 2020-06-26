2ba0e423274063baf4d5a954355906f4a159cf86
saber <saberfathallah7@gmail.com>
2020-06-26 14:59:43 +0200

    config

---
+++ src/components/EditNinja/EditNinja.jsx
@@ -0,0 +1,58 @@
+import React from "react";
+import axios from "axios"
+class EditNinja extends React.Component {
+  state = {
+    name: '',
+    rank: '',
+    loadNinja: true,
+  }
+
+  componentDidMount () {
+    console.log("props", this.props.match.params.id)
+    axios.get(`http://localhost:4000/api/ninjas/${this.props.match.params.id}`)
+    .then(response => {
+      const data = response.data;
+      console.log("data", data)
+      this.setState({ name: data.ninja.name, rank: data.ninja.rank, loadNinjas: false })
+    })
+  }
+
+  onSubmit = (e) => {
+    e.preventDefault();
+
+    const ninja = {
+      name: this.state.name,
+      rank: this.state.rank,
+    }
+
+    axios.put(`http://localhost:4000/api/ninjas/${this.props.match.params.id}`, ninja)
+    .then(() => {
+      this.props.history.push("/")
+    });
+  }
+
+  handleInputChange = (event) => {
+    const { name, value } = event.target;
+    this.setState({ [name]: value });
+  };
+
+  
+  render() {
+    const {
+      name,
+      rank,
+    } = this.state;
+    return (
+      <div>
+
+       <form onSubmit={this.onSubmit}>
+        <input type="text" name="name" value={name} onChange={this.handleInputChange}/>
+        <input type="text" name="rank" value={rank} onChange={this.handleInputChange}/>
+        <button variant="success" type="submit"> edit ninja</button> 
+      </form>
+      </div>
+    )
+  }
+}
+
+export default EditNinja;
---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Header = () => 
+  (
+    <div>
+        <p>homme</p>
+        <p>contact</p>
+        <p>products</p>
+    </div>
+  )
+
+
+export default Header;
